0bb00a9e7e4cf1373b7fcd64c979a0d2de8f72c5
Célelo <mariaceleste.lopez41@gmail.com>
2024-02-17 22:36:17 -0300

    adding js url base

--- script.js
+++ script.js
@@ -2,4 +2,23 @@
 
 const $ = (selector) => document.querySelector(selector)
 
-const url = 'http://gateway.marvel.com/v1/public/'
+const urlBase = `http://gateway.marvel.com/v1/public/`
+let ts = 1
+const publicKey = '54b254b30e4102f6835ccaf9fc8daf8a'
+const hash = '84fbd4beafba7037582101c9400db27a'
+
+const getMarvel = async (resource) => {
+    if (resource !== 'comics' && resource !== 'characters') {
+        console.log('Recurso no válido. Debe ser "comics" o "characters".')
+    }
+    try {
+        let url = `${urlBase}${resource}?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        return data.data.results
+    } catch (error) {
+        
+        console.log(error)
+    }
+}
